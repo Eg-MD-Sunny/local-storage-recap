2a0a3132c4bed99b3afda432495818cedac548e3
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-02-02 00:02:30 +0600

    Card Design

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -7,7 +7,7 @@ const Product = (props) => {
         <div className="product-style">
             <h3>ID: {id}</h3>
             <p>Name: {name}</p>
-            <h4>Price: {price}</h4>
+            <h4>Price: ${price} USD</h4>
             <button style={{padding:'3px 7px'}}>Add To Click</button>
         </div>
     );
