40ce5cabd66532bf634ccdb759864c001df1cbf5
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-02-02 00:08:54 +0600

    Modify

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { addDB } from '../Utilities/Utilities,js';
 import './Product.css';
 
 const Product = (props) => {
     const {id,name,price}=props.sendingProductData;
+    const addToClick = (id) =>{
+        addDB(id);
+    }
     return (
         <div className="product-style">
             <h3>ID: {id}</h3>
             <p>Name: {name}</p>
             <h4>Price: ${price} USD</h4>
-            <button style={{padding:'3px 7px'}}>Add To Click</button>
+            <button style={{padding:'3px 7px'}} onClick={()=>addToClick(id)}>Add To Click</button>
         </div>
     );
 };
