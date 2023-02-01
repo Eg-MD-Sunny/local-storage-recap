eb21f13b50b8c06c92eafc4c51a15b2823fa53bf
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-02-01 22:54:26 +0600

    Local Storage

---
+++ src/Component/Products/Products.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const Products = () => {
+    const [products,setProducts] = useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
+    return (
+        <div>
+            <h1>Product Loaded: {products.length}</h1>
+            {
+                
+            }
+        </div>
+    );
+};
+
+export default Products;
