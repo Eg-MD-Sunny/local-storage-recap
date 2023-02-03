5d1fdf6066cf4a592116b5d3e3d68daf6b42119e
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2023-02-03 23:30:50 +0600

    Modify

--- src/Component/Utilities/fakeDB.js
+++ src/Component/Utilities/fakeDB.js
@@ -1,14 +1,26 @@
 const addToDb = (id) =>{
-    const matchDataId = localStorage.getItem(id)
-    //console.log('Geeting Data Type', typeof matchDataId);
-    if(matchDataId){
-        const newValueSet= matchDataId + 1;
-    //console.log('Increase Data Type', typeof newValueSet);
-        localStorage.setItem(id,newValueSet);
-    }else{
-        localStorage.setItem(id,1)
-    }
+    let shoppingCart ={}
+    console.log(typeof shoppingCart)
+    localStorage.setItem('data-store',JSON.stringify(shoppingCart))
+
+    let matchDataId = shoppingCart[id]
+    
+
+
+
+
+
+
+    // const matchDataId = localStorage.getItem(id)
+    // //console.log('Geeting Data Type', typeof matchDataId);
+    // if(matchDataId){
+    //     const newValueSet= parseInt(matchDataId) + 1;
+    // //console.log('Increase Data Type', typeof newValueSet);
+    //     //localStorage.setItem(id,newValueSet);
+    // }else{
+    //     //localStorage.setItem(id,1)
+    // }
 }
 
-export { addToDb };
+export { addToDb }
 
